a881a73d1bb9540ed3e540f54b753c122de285ff
tackcanada <153316087+tackcanada@users.noreply.github.com>
2023-12-09 16:34:24 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,26 @@
 let slideIndex = 0;
 
-// Function to display slides
 function showSlides() {
   let i;
   const slides = document.getElementsByClassName("slides");
   const images = slides[0].getElementsByTagName("img");
 
-  // Hide all images
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
   }
 
-  // Increment slide index and display the next image
   slideIndex++;
   if (slideIndex > images.length) {
     slideIndex = 1;
   }
   images[slideIndex - 1].style.display = "block";
 
-  // Change image every 2 seconds (adjust as needed)
-  setTimeout(showSlides, 2000);
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-// Function to change slides when clicking next/previous buttons
 function changeSlide(n) {
   slideIndex += n;
   showSlides();
 }
 
-// Initially start the slideshow
-showSlides();
+showSlides(); // Initially start the slideshow

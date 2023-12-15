4a3f45eead3be9c1da2e30242ba77ea77de83b78
tackcanada <153316087+tackcanada@users.noreply.github.com>
2023-12-15 12:56:19 -0500

    Added fade animations

--- docs/script.js
+++ docs/script.js
@@ -15,7 +15,7 @@ function showSlides() {
   }
   images[slideIndex - 1].style.display = "block";
 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 function changeSlide(n) {

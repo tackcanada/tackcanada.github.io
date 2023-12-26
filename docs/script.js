a6ac8132c6fc559eb6d370926e07022a2063a862
tackcanada <george.zeng.canada@gmail.com>
2023-12-26 17:53:38 -0500

    Updated slide show and nav bar

--- docs/script.js
+++ docs/script.js
@@ -7,3 +7,35 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+/* Slideshow script */
+let slideIndex = 1; /* Slide counter */
+showSlides(slideIndex);
+
+function plusSlides(n) { /* Manual switch slides */
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) { /* How the slides will be displayed/manipulated */
+  let i;
+  const slides = document.getElementsByClassName("slide"); /* slides constant */
+  const buttons = document.getElementsByClassName("slide-button"); /* buttons constant */
+  
+  if (n > slides.length) { slideIndex = 1; } /* Loops the slides if user goes forward too much */
+  if (n < 1) { slideIndex = slides.length; } /* Loops the slides if user goes backward too much */
+  
+  for (i = 0; i < slides.length; i++) { /* Hide all the slides by default */
+    slides[i].style.display = "none";
+  }
+  
+  for (i = 0; i < buttons.length; i++) { /* Display only the slide instructed by slideIndex variable */
+    buttons[i].classList.remove("active");
+  }
+  
+  slides[slideIndex - 1].style.display = "block"; 
+  buttons[slideIndex - 1].classList.add("active");
+}
+
+function slideButtonClick(n) {
+  showSlides(slideIndex = n);
+}
